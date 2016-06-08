ed3008b7403dd07465446b8911c19bd3080846ff
Clarissa Rodrigues <clarissa.ar@gmail.com>
2016-06-08 15:55:22 -0300

    Innitial commit.
    Signed-off-by: Clarissa Rodrigues <clarissa.ar@gmail.com>

---
+++ test/getBase_e2e.js
@@ -0,0 +1,49 @@
+var assert = require('assert');
+var webdriver = require('selenium-webdriver');
+var LoginPage = require('../lib/LoginPage.js');
+var LeadsPage = require('../lib/LeadsPage.js');
+var SettingsPage = require('../lib/SettingsPage.js');
+
+describe('getBase App', function () {
+    this.timeout(60000);
+    var driver;
+
+    before(function () {
+        driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.firefox()).build();
+        driver.manage().window().maximize();
+
+    });
+
+    after(function () {
+        // driver.quit();
+    });
+
+    it('should login successfully', function (done) {
+
+        LoginPage.login(driver, 'emailtest657@gmail.com', 'emailtest6572016');
+
+        driver.call(function () {
+            done();
+        });
+    });
+
+    it('should create a new lead', function (done) {
+
+        LeadsPage.createLead(driver, 'John', 'M', 'Comp', 'New');
+
+        driver.call(function () {
+            done();
+        });
+    });
+
+
+    it('should change from "New" to "ChangedNew" Label on Lead Statuses Page', function (done) {
+
+        SettingsPage.changeLeadStatuses(driver, 'New', 'ChangedNew');
+
+
+        driver.call(function () {
+            done();
+        });
+    });
+});
